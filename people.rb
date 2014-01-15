63149b0cf6c9455649f953c01db2e7e0078dd502
nizarith <nizarith@live.com>
2014-01-15 02:01:32 -0330

    first test run

--- people.rb
+++ people.rb
@@ -1,17 +1,32 @@
 class Parent
-	attribute :name
-	puts ""
+  attr_name :name
+  attr_greet :greeting
+
+  def greeting
+    "Hello, my name is #{name}."
+  end
+
 end
 
 
-class Student < parent
-	def learn
-		puts "I get it!"
-	end
+class Student < Parent
+
+  def learn
+    puts "I get it!"
+  end
 end
 
-class Instructor < parent
-	def teach
-		puts "Everything in Ruby is an Object"
-	end
+student = Student.new
+student.name = "Christina"
+puts student.greeting
+
+
+class Instructor < Parent
+  def teach
+    puts "Everything in Ruby is an Object"
+  end
 end
+
+instructor = Instructor.new
+instructor.name = "Chris"
+puts instructor.greeting
