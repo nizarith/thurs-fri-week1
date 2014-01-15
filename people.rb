d7591f64fa45882a2cfd12dc427d00bc3ba439d9
nizarith <nizarith@live.com>
2014-01-15 13:22:30 -0330

    finished people and updated sales tax app w/ sales tax and import tax

--- people.rb
+++ people.rb
@@ -4,7 +4,6 @@ class Person
   def greeting
     puts "Hello, my name is #{name}."
   end
-
 end
 
 class Student < Person
--- sales_tax_app.rb
+++ sales_tax_app.rb
@@ -1,12 +1,49 @@
+class Goods
+  attr_accessor :imported; :exemptst; :cost;
+
+  def sales_tax
+  	unless @exemptst
+  		return "#{cost}" * 0.10
+  	else
+  		return "#{cost}"
+  	end
+  end
+
+  def import_tax
+  	unless @imported
+  		return "#{cost}" * 0.05
+  	else
+  		return "#{cost}"
+  	end
+  end
+
+
+
 puts "Input 1\:"
 puts "1 book at 12.49"
 puts "1 music CD at 14.99"
 puts "1 chocolate bar at 0.85"
+
+book1 = Goods.new
+book1
  
+
+
+
+
+
+
 puts "Input 2\:"
 puts "1 imported box of chocolates at 10.00"
 puts "1 imported bottle of perfume at 47.50"
  
+
+
+
+
+
+
+
 puts "Input 3\:"
 puts "1 imported bottle of perfume at 27.99"
 puts "1 bottle of perfume at 18.99"
